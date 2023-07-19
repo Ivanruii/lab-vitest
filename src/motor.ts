41ee1374e5aae8ff98e9a613b2887c0d5d92d861
Ivanruii <ivr.644@gmail.com>
2023-07-19 02:08:01 +0200

    Added motor.ts and implemented

---
+++ src/motor.ts
@@ -0,0 +1,38 @@
+import { scoreToWin, gameData, cardsOfDeck } from './model'
+import { gameWinned, resetUI, gameOver } from './ui'
+
+
+interface Card {
+    name: string;
+    value: number;
+    url: string;
+}
+
+function resetGame() {
+    resetUI()
+    gameData.cardsInPiles = 40;
+    gameData.roundScore = 0;
+}
+
+function updateGame(pickedCard: Card) {
+    gameData.cardsInPiles -= 1;
+    gameData.roundScore += pickedCard.value;
+    gameData.totalGameScore += pickedCard.value;
+    if (gameData.roundScore > scoreToWin) {
+        gameOver();
+        resetGame()
+    } else {
+        gameWinned();
+    }
+}
+
+function giveRandomCard(): Card {
+    const randomIndex = Math.floor(Math.random() * cardsOfDeck.length);
+    const pickedCard = cardsOfDeck[randomIndex];
+
+    gameData.cardsInPiles -= 1;
+    return pickedCard;
+}
+
+
+export { giveRandomCard, updateGame}
